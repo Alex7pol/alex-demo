50a061fac92edec36062bf86ee75c0023acaa2fe
Alex Polischuk <alex7pol@gmail.com>
2025-09-25 22:36:07 +0300

    Initial commit

---
+++ FillerFile.java
@@ -0,0 +1,12 @@
+// This file is generated to meet the minimum size requirement for license scanning.
+// It contains more than 300 non-whitespace characters.
+
+public class FillerFile {
+    public static void main(String[] args) {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < 50; i++) {
+            sb.append("This is a filler line to increase the file size for license scanning.\n");
+        }
+        System.out.println(sb.toString());
+    }
+}
---
+++ Java_HelloWorld.java
@@ -0,0 +1,14 @@
+
+public class Java_HelloWorld {
+ 
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        int a=0;
+        a = 8;
+        if (9/0!=0)
+        {
+            System.out.println("Divide by zero");
+        }
+        System.out.println(args);
+    }
+}
